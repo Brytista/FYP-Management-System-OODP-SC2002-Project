485325aa7c584cd5e42e667a244d15356ee4754b
JvThunder <joshuaadrianc@gmail.com>
2023-04-01 21:23:57 +0800

    v1

--- SC2002Project/src/package1/DisplayAll.java
+++ SC2002Project/src/package1/DisplayAll.java
@@ -2,8 +2,14 @@ package package1;
 import java.util.List;
 
 public class DisplayAll implements DisplayRequest {
-    void DisplayRequestHistory(List<Request> requestHistory) {
+    public void DisplayRequestHistory(List<Request> requestHistory) {
         for(Request request : requestHistory) {
+            System.out.println(request);
+        }
+    }
+
+    public void DisplayPendingRequest(List<Request> pendingRequest) {
+        for(Request request : pendingRequest) {
             System.out.println(request);
         }
     }
